d48bdbaaa374bb935a6b38f0ed16c7b65adb37d0
carinach <mllecarinach@gmail.com>
2017-01-09 16:16:07 +0100

    Initial commit

---
+++ main.js
@@ -0,0 +1,15 @@
+var x = [1, 2, 3, 4, 5];
+var y = [1, 2, 3, 4, 5];
+
+
+tirageX(){
+    for (var i = 0; i < x.length; i++) {
+        x[i]
+    }
+}
+
+tirageY(){
+    for (var i = 0; i < y.length; i++) {
+        y[i]
+    }
+}
