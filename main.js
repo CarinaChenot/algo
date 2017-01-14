c25a3733278321d3bb51ca2b8c969c31ea2aa15e
lucillef <lucille@MacBook-Pro-de-Lucille.local>
2017-01-14 16:54:38 +0100

    Tris des des

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 var lancer = document.querySelector('button');
 var dices = document.getElementsByClassName('dice');
+var rank = 0;
 
 /* Lancer les dés au clic */
 lancer.addEventListener('click', function() {
@@ -8,6 +9,11 @@ lancer.addEventListener('click', function() {
     var tempX;
     var tempY;
     var ok = false;
+    rank = 0;
+    for(var i = 0; i<5; i++){           //on enlève la classe de tri pour pouvoir trier si on relance sans recharger la page
+        dices[i].classList.remove()
+    }
+
     for (var i = 0; i < 5; i++) {
         do {
             tempX = Math.round(Math.random() * 4 + 1); /* Tirer une position x et y aléatoire */
@@ -21,6 +27,8 @@ lancer.addEventListener('click', function() {
         }
     }
     giveClasses(dices, posX, posY); /* On attribue les classes en fonction des positions */
+    addNumber(dices);
+    shorting(dices);
 });
 
 /* Vérifier si la position n'est pas déjà prise */
@@ -43,4 +51,22 @@ function giveClasses(dices, posX, posY) {
     }
 }
 
+/* Attribuer une valeur au dées */
+function addNumber(dices){
+    for (var i = 0; i<5; i++){
+        var number = Math.floor(Math.random() * 5 + 1);
+        console.log(number)
+        dices[i].innerHTML = number;
+    }
+}
 /* Au clic sur les dés ça affecte les classes h0, h1, h2, h3, h4 */
+
+function shorting(dices) {
+    for (var j=0; j<5; j++){
+        dices[j].addEventListener('click', function(){
+            this.style.transform = 'rotate(0)';
+            this.classList.add('h'+ rank);
+            rank++;
+        });
+    }
+}
